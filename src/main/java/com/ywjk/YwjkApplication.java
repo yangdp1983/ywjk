3c4a90968e5b0ade9660d425ebd92ac3740a24e1
yangdp1983 <741744094@qq.com>
2020-09-06 11:28:14 +0800

    Initial commit

---
+++ src/main/java/com/ywjk/YwjkApplication.java
@@ -0,0 +1,13 @@
+package com.ywjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwjkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YwjkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ywjk/YwjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwjkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
